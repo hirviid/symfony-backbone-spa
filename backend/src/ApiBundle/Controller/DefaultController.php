3d559da98a3f3e710670930fe56b573664a780f4
David Van Gompel <david.vangompel@nivario.com>
2015-01-09 23:06:24 +0100

    removed dummy content

--- backend/src/ApiBundle/Controller/DefaultController.php
+++ backend/src/ApiBundle/Controller/DefaultController.php
@@ -16,13 +16,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $repository =  $this->get('time_slot_repository');
-        $repository->add(
-            new TimeSlot(new TimeSlotId('62A0CEB4-0403-4AA6-A6CD-1EE808AD4D23'), new \DateTime('2015-01-01 09:00'), new \DateTime('2015-01-01 10:00'))
-        );
-        $repository->add(
-            new TimeSlot(new TimeSlotId('62A0CEB4-0403-4AA6-A6CD-1EE808AD4D44'), new \DateTime('2015-01-01 10:00'), new \DateTime('2015-01-01 10:45'))
-        );
         return $this->render('default/index.html.twig');
     }
 } 

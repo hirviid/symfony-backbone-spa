8315d62a1f6c0e6704010d72ccbeea3fdec145df
David Van Gompel <david.vangompel@nivario.com>
2015-01-09 23:59:31 +0100

    added command

---
+++ backend/src/ApiBundle/Commands/TrackTimeCommand.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david_000
+ * Date: 9/01/2015
+ * Time: 23:58
+ */
+
+namespace ApiBundle\Commands;
+
+
+class TrackTimeCommand 
+{
+    /**
+     * @var \DateTime
+     */
+    public $startedAt;
+
+    /**
+     * @var \DateTime
+     */
+    public $stoppedAt;
+} 
--- backend/src/ApiBundle/Controller/DefaultController.php
+++ backend/src/ApiBundle/Controller/DefaultController.php
@@ -11,11 +11,17 @@ namespace ApiBundle\Controller;
 use CoreDomain\TimeSlot\TimeSlot;
 use CoreDomain\TimeSlot\TimeSlotId;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        /*(new Filesystem())->copy(
+            __DIR__ . '/../Tests/Fixtures/time_slots.yml',
+            $this->get('kernel')->getCacheDir() . '/time_slots.yml',
+            true
+        );*/
         return $this->render('default/index.html.twig');
     }
 } 

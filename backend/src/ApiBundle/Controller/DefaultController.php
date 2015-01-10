fbd69596e200eedc0886b96383dd849b559e2c0c
David Van Gompel <david.vangompel@nivario.com>
2015-01-10 17:28:38 +0100

    use redis for session storage

--- backend/src/ApiBundle/Controller/DefaultController.php
+++ backend/src/ApiBundle/Controller/DefaultController.php
@@ -12,16 +12,18 @@ use CoreDomain\TimeSlot\TimeSlot;
 use CoreDomain\TimeSlot\TimeSlotId;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         /*(new Filesystem())->copy(
             __DIR__ . '/../Tests/Fixtures/time_slots.yml',
             $this->get('kernel')->getCacheDir() . '/time_slots.yml',
             true
         );*/
+
         return $this->render('default/index.html.twig');
     }
 } 
